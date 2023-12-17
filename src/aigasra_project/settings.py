c30491e7a961c701826b4ae032e24588755dc8a8
Anderson Ocaña <86977326+Andru-1987@users.noreply.github.com>
2023-12-17 15:26:02 -0300

    Update settings.py

--- src/aigasra_project/settings.py
+++ src/aigasra_project/settings.py
@@ -143,7 +143,7 @@ STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = None if DEBUG else BASE_DIR / "staticfiles"
 
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
