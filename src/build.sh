bb0c08f159c71dca117e99fd2b446ceff6b64b5b
andru-oca <anderson.ocana@bue.edu.ar>
2023-12-18 10:39:17 -0300

    render changes

--- src/build.sh
+++ src/build.sh
@@ -13,7 +13,7 @@ python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
 
-DJANGO_SUPERUSER_USERNAME=11111111 \
-DJANGO_SUPERUSER_PASSWORD=admin \
-DJANGO_SUPERUSER_EMAIL="mail_admin@mail.com" \
-python manage.py createsuperuser --dni 11111111 --noinput
+# DJANGO_SUPERUSER_USERNAME=11111111 \
+# DJANGO_SUPERUSER_PASSWORD=admin \
+# DJANGO_SUPERUSER_EMAIL="mail_admin@mail.com" \
+# python manage.py createsuperuser --dni 11111111 --noinput
